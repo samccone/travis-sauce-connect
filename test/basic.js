1a1f9da35fba80e60fa2485564bdba0ce4d9313d
Sam Saccone <samccone@gmail.com>
2015-03-17 18:59:03 -0400

    Add basic selenium test example

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,28 @@
-var assert = require('assert');
-describe("a basic test", function() {
-  it("should be ok", function() {
-    assert.equal(1, 1);
+var assert = require("assert");
+var webdriver = require("selenium-webdriver");
+
+describe("testing javascript in the browser", function() {
+  beforeEach(function() {
+    this.browser = new webdriver.Builder()
+    .withCapabilities({
+      browserName: "chrome"
+    }).build();
+
+    return this.browser.get("http://localhost:8000/page/index.html");
+  });
+
+  afterEach(function() {
+    return this.browser.quit();
+  });
+
+  it("should handle clicking on a headline", function(done) {
+    var headline = this.browser.findElement(webdriver.By.css('h1'));
+
+    headline.click();
+
+    headline.getText().then(function(txt) {
+      assert.equal(txt, "awesome");
+      done();
+    });
   });
 });
