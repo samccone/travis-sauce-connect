66f35e2380aebc0abdb85d365731d012b442d7fa
Sam Saccone <samccone@gmail.com>
2015-03-17 19:59:08 -0400

    Enable running tests on sauce-connect :boom:

--- test/basic.js
+++ test/basic.js
@@ -3,10 +3,22 @@ var webdriver = require("selenium-webdriver");
 
 describe("testing javascript in the browser", function() {
   beforeEach(function() {
-    this.browser = new webdriver.Builder()
-    .withCapabilities({
-      browserName: "chrome"
-    }).build();
+    if (process.env.SAUCE_USERNAME != undefined) {
+      this.browser = new webdriver.Builder()
+      .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
+      .withCapabilities({
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        username: process.env.SAUCE_USERNAME,
+        accessKey: process.env.SAUCE_ACCESS_KEY,
+        browserName: "chrome"
+      }).build();
+    } else {
+      this.browser = new webdriver.Builder()
+      .withCapabilities({
+        browserName: "chrome"
+      }).build();
+    }
 
     return this.browser.get("http://localhost:8000/page/index.html");
   });
